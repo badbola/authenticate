7a7a06d8d5845acb80dc7a6a0a2fab348d4b8270
Divyanshu <38465652+badbola@users.noreply.github.com>
2020-06-28 13:36:19 +0530

    Added mangoose and layouts

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
+//express server
 const express = require('express');
-const { urlencoded } = require('express');
+//port of localhost on which web app will run
 const port = 8000;
-
+//implementing db in the index
 const db = require('./config/mongoose');
-
+//assingning all the libraries of express to a const app
 const app = express();
+// implementing express layouts
+const expressLayouts = require('express-ejs-layouts');
+// using express layouts
+app.use(expressLayouts);
+//for seperate js and css for every page
+app.set('layout extractStyles',true);
+app.set('layout extractScripts', true);
+
+//setting up view engine
 app.set('view engine', 'ejs');
+//assingning folder of ejs files to views
 app.set('views', 'views');
 app.use(express.urlencoded());
-app.use(express.static('assets'));
-// function requestHandler(req,res){
-//     console.log(req.url);
-//     res.writeHead(200,{'content-type': 'text/html'});
-//     res.end("<h1>And we start</h1>");
-// }
-app.get('/',function(req,res){
-    return res.render('home',{
-        title: 'Home is here'
-    });
-})
 
+// for implementing css and js files to web app
+app.use(express.static('assets'));
+//use express router
+app.use('/',require('./routes/index'));
+
+
+//listing the port and check whether running or not
 app.listen(port,function(err){
     if(err){
         console.log('Initial Error', err);
